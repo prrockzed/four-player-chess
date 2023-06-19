a09392d8030b84126c27367d9e8a906b7fe2f0e7
prrockzed <prrockzed@pm.me>
2023-06-19 20:05:53 +0530

    Deleted unused files

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +12,3 @@ root.render(
   </React.StrictMode>
 );
 
-// To start measuring performance in the app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
