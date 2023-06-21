3465a36aaf0b907af478ae53a6fe32f4763ef705
prrockzed <prrockzed@pm.me>
2023-06-21 14:45:22 +0530

    Added basic pawn rules for all colors of pawn
    Attacking and capturing pieces are still not allowed

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -26,6 +26,36 @@ export default class Referee {
             return true
           }
         }
+      } else if (team === TeamType.YELLOW) {
+        if (py === 12) {
+          if (px === x && (y - py === -1 || y - py === -2)) {
+            return true
+          }
+        } else {
+          if (px === x && y - py === -1) {
+            return true
+          }
+        }
+      } else if (team === TeamType.BLUE) {
+        if (px === 1) {
+          if (py === y && (x - px === 1 || x - px === 2)) {
+            return true
+          }
+        } else {
+          if (py === y && x - px === 1) {
+            return true
+          }
+        }
+      } else {
+        if (px === 12) {
+          if (py === y && (x - px === -1 || x - px === -2)) {
+            return true
+          }
+        } else {
+          if (py === y && x - px === -1) {
+            return true
+          }
+        }
       }
     }
     return false
