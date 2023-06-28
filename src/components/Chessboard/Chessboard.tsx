7db39cbc5159918c9439116e023e12880f949de0
prrockzed <prrockzed@pm.me>
2023-06-28 10:00:01 +0530

    Pawn promotion done
    Pawns are now promoted on the 8th row as in a 2 player chess game

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -170,8 +170,63 @@ export default function Chessboard() {
   }
 
   function promotePawn(pieceType: PieceType) {
-    console.log('Pawn is promoting into ' + pieceType)
-    console.log(promotePawn)
+    if (promotionPawn === undefined) {
+      return
+    }
+
+    const updatedPieces = pieces.reduce((results, piece) => {
+      if (samePosition(piece.position, promotionPawn.position)) {
+        piece.type = pieceType
+
+        let teamType = ''
+        if (piece.team === TeamType.RED) {
+          teamType = 'r'
+        } else if (piece.team === TeamType.BLUE) {
+          teamType = 'b'
+        } else if (piece.team === TeamType.YELLOW) {
+          teamType = 'y'
+        } else if (piece.team === TeamType.GREEN) {
+          teamType = 'g'
+        }
+
+        let image = ''
+
+        switch (pieceType) {
+          case PieceType.ROOK:
+            image = 'R'
+            break
+          case PieceType.KNIGHT:
+            image = 'N'
+            break
+          case PieceType.BISHOP:
+            image = 'B'
+            break
+          case PieceType.QUEEN:
+            image = 'Q'
+            break
+        }
+
+        piece.image = `assets/images/${teamType}${image}.png`
+      }
+      results.push(piece)
+      return results
+    }, [] as Piece[])
+
+    setPieces(updatedPieces)
+
+    modalRef.current?.classList.add('hidden')
+  }
+
+  function promotionTeamType() {
+    if (promotionPawn?.team === TeamType.RED) {
+      return 'r'
+    } else if (promotionPawn?.team === TeamType.BLUE) {
+      return 'b'
+    } else if (promotionPawn?.team === TeamType.YELLOW) {
+      return 'y'
+    } else if (promotionPawn?.team === TeamType.GREEN) {
+      return 'g'
+    }
   }
 
   let board = []
@@ -196,22 +251,22 @@ export default function Chessboard() {
         <div className='modal-body'>
           <img
             onClick={() => promotePawn(PieceType.ROOK)}
-            src='/assets/images/yR.png'
+            src={`/assets/images/${promotionTeamType()}R.png`}
             alt='Rook'
           />
           <img
             onClick={() => promotePawn(PieceType.KNIGHT)}
-            src='/assets/images/yN.png'
+            src={`/assets/images/${promotionTeamType()}N.png`}
             alt='Knight'
           />
           <img
             onClick={() => promotePawn(PieceType.BISHOP)}
-            src='/assets/images/yB.png'
+            src={`/assets/images/${promotionTeamType()}B.png`}
             alt='Bishop'
           />
           <img
             onClick={() => promotePawn(PieceType.QUEEN)}
-            src='/assets/images/yQ.png'
+            src={`/assets/images/${promotionTeamType()}Q.png`}
             alt='Queen'
           />
         </div>
